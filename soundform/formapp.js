817403a8bf1ccb979d47a3c69c44c1464b692bd8
Alexander Wttewaall <alexwttewaall@gmail.com>
2016-03-17 16:40:30 +0100

    soundform

---
+++ soundform/formapp.js
@@ -0,0 +1,36 @@
+angular.module('formApp', ['ngAnimate', 'ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider
+    
+        .state('form', {
+            url: '/form',
+            templateUrl: 'form.html',
+            controller: 'formController'
+        })
+        .state('form.soundform', {
+            url: '/soundform',
+            templateUrl: 'soundform.html'
+        })
+        
+        .state('form.infoform', {
+            url: '/infoform',
+            templateUrl: 'infoform.html'
+        })
+
+        .state('form.addform', {
+            url: '/addform',
+            templateUrl: 'addform.html'
+        });
+       
+    $urlRouterProvider.otherwise('/form/soundform');
+})
+
+.controller('formController', function($scope) {
+    
+    $scope.formData = {};
+    $scope.processForm = function() {
+        alert('awesome!');  
+    };
+    
+});
